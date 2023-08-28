5d9b9db562316d5f4f091f6bae8eed30623e690f
Jorge <jorge.garcia.pastor@gmail.com>
2023-08-28 12:57:40 +0200

    minor changes on url img

--- src/Page.tsx
+++ src/Page.tsx
@@ -40,7 +40,7 @@ const Page = () => {
 return (
   <>
     <div className='front-img'>
-      <img src='../src/assets/img/logo_hr.svg' className="logo" alt="logo horarios" />
+      <img src='./src/assets/img/logo_hr.svg' className="logo" alt="logo horarios" />
     </div>
     <div className='main_page'>
         <div className='head_page'>
