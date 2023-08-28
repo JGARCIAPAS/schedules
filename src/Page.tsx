843b29c6d416243f774bb3136dd6ae1a3e3dccce
Jorge <jorge.garcia.pastor@gmail.com>
2023-08-28 14:33:05 +0200

    reste changes

--- src/Page.tsx
+++ src/Page.tsx
@@ -40,7 +40,7 @@ const Page = () => {
 return (
   <>
     <div className='front-img'>
-      <img src='./src/assets/img/logohr.svg' className="logo" alt="logo horarios" />
+      <img src='../src/assets/img/logohr.png' className="logo" alt="logo horarios" />
     </div>
     <div className='main_page'>
         <div className='head_page'>
